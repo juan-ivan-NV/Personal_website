d3f2dc091909f8f66e24359f28d7e20ca0bb9e63
juan-ivan-NV <coldcram14@hotmail.com>
2021-01-04 17:34:56 -0600

    jinbsite working to send mails

--- Version2/jinbsite/contact/views.py
+++ Version2/jinbsite/contact/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.core.mail import send_mail
 # Create your views here.
 
 def contact(request):
-    contact_form=ContactForm()
-    return render(request,"contact/contact.html",{'myForm':contact_form})
+
+    '''if request.method =="POST":
+
+        myForm = ContactForm(request.POST)
+
+        if myForm.is_valid():
+            
+            infForm=myForm.cleaned_data
+
+            send_mail(infForm['Name'], infForm['Content'], infForm.get('Email',''), ['coldcram14@hotmail.com'],)
+
+            return render(request, 'contact.html')
+
+    else:''' 
+
+    myForm = ContactForm()
+
+    return render(request, 'contact/contact.html', {'form': myForm})
--- Version2/jinbsite/jinbsite/settings.py
+++ Version2/jinbsite/jinbsite/settings.py
@@ -127,3 +127,10 @@ import os
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="juanivannb142@gmail.com" 
+EMAIL_HOST_PASSWORD="..."
