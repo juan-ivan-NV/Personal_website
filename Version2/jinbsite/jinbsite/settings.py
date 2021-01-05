89095fa9a5314892feff82f4a1cffb920e775049
juan-ivan-NV <coldcram14@hotmail.com>
2021-01-05 16:06:06 -0600

    jinbsite 2nd project and pg db

--- Version2/jinbsite/jinbsite/settings.py
+++ Version2/jinbsite/jinbsite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'jinbsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'jinbsite_db',
+        'USER' : 'postgres',
+        'PASSWORD' : '.....',
+        'HOST' : '127.0.0.1',
+        'DATABASE_PORT' : '5432',
     }
 }
 
