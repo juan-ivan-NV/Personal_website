296611a6db396515770bc958ce784ab0b2bb1313
juan-ivan-NV <coldcram14@hotmail.com>
2021-02-07 20:56:15 -0600

    dumping data to production

--- Version2/jinbsite/jinbsite/settings.py
+++ Version2/jinbsite/jinbsite/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'jinbsite_db',
         'USER' : 'postgres',
-        'PASSWORD' : 'xxxxxxxx',
+        'PASSWORD' : 'xxxxxxx',
         'HOST' : '127.0.0.1',
         'DATABASE_PORT' : '5432',
     }
